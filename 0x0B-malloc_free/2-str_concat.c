49f7ea1a9d920727a9b7e4ba3d36fbc5bc949d77
Dawit-Bekalu <dawbekalu1234@gmail.com>
2023-01-09 12:16:24 -0800

    last commited

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *strout;
 
 	unsigned int i, j, k, limit;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-	       s2 = "";
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
@@ -24,10 +25,10 @@ char *str_concat(char *s1, char *s2)
 	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
-		{
-			free(strout);
-			return (NULL);
-		}
+	{
+		free(strout);
+		return (NULL);
+	}
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 	limit = j;
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,5 @@
-
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: width of the array.
@@ -22,17 +20,17 @@ int **alloc_grid(int width, int height)
 		free(gridout);
 		return (NULL);
 	}
-	for(i = 0; i< height; i++)
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
 		{
-			gridout[i] = malloc(width * sizeof(int));
-			if (gridout[i] == NULL)
-			{
-				for (i--; i >= 0; i--)
-					free(gridout[i]);
-				free(gridout);
-				return (NULL);
-			}
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
 		}
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			gridout[i][j] = 0;
